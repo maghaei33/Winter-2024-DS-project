2acfd8519d0f15f2f8d3a175306c77f70591378b
Mahdi Aghaei <52649502+maghaei33@users.noreply.github.com>
2024-01-19 16:43:22 +0330

    Add files via upload

--- rproject.cpp
+++ rproject.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+struct node
+{
+    node* children;
+    node* brosis;
+    int name;
+};
+node* head = new node();
+
 int main()
 {
+    while(true)
+    {
+        cout<<"Welcome to Family Tree!\n";
+        cout << "Choose appropriate number\n";
+        cout<<"1) Add a new member to tree\n";
+        cout<<"2) Remove a member from tree\n";
+        int t;
+        cin >> t;
+    }
     return 0;
 }

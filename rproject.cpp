64976c3a3b7b0adaac091573471dfb93db80e5ef
maghaei33 <m.a.aghaei.33@gmail.com>
2024-01-21 12:11:13 +0330

    changed starting

--- rproject.cpp
+++ rproject.cpp
@@ -1,51 +1,74 @@
 
 #include<iostream>
 #include<string>
-#include<vector>
 
 using namespace std;
 
 struct node
 {
+    node* father;
     node* bigchild;
     node* brosis;
     string name;
 };
 node* head = new node();
 
-vector <string> father;
-vector <string>child;
-vector <node>id;
+//size function
+int size(int t , node* p)
+{
+    if(p->bigchild!=NULL && p->brosis==NULL)
+    {
+        t++;
+        size(t,p->bigchild);
+    }
+    if(p->bigchild==NULL && p->brosis!=NULL)
+    {
+        t++;
+        size(t,p->brosis); 
+    }
+    if(p->bigchild==NULL && p->brosis==NULL)
+    {
+        t++;
+        t++;
+        size(t,p->bigchild);
+        size(t,p->brosis); 
+    }
+    return t;
+}
 
-void start(string member)
+//delete function
+void deleten (string x)
 {
-    head->name=member;
-}
-void isnodehere (string x , node* p , int t)
-{
-    if(p->name==x)
+    node* p=new node();
+    p = find(x,head);
+    if(p->father->bigchild==p)
     {
-        t=1;
+        if(p->brosis!=NULL)
+        {
+            p->father->bigchild = p->brosis;
+        }
+        else
+        {
+            p->father->bigchild=NULL;
+        }
+
     }
-    else
+    if(p->father->brosis==p)
     {
-        if(p->bigchild!=NULL && p->brosis==NULL)
+        if(p->brosis!=NULL)
         {
-            isnodehere(x, p->bigchild , t);
+            p->father->brosis = p->brosis;
         }
-        if(p->bigchild==NULL && p->brosis!=NULL)
+        else
         {
-            isnodehere(x, p->brosis , t);
+            p->father->brosis=NULL;
         }
-        if(p->bigchild!=NULL && p->brosis!=NULL)
-        {
-            isnodehere(x, p->brosis , t);
-            isnodehere(x, p->bigchild , t);  
-        }
+
     }
-    
+
 }
-node* foundnode(string x , node* p)
+//find finction
+node* find(string x , node* p)
 {
     if(p->name==x)
     {
@@ -55,27 +78,31 @@ node* foundnode(string x , node* p)
     {
         if(p->bigchild!=NULL && p->brosis==NULL)
         {
-            foundnode(x, p->bigchild);
+            find(x, p->bigchild);
         }
         if(p->bigchild==NULL && p->brosis!=NULL)
         {
-            foundnode(x, p->brosis);
+            find(x, p->brosis);
         }
         if(p->bigchild!=NULL && p->brosis!=NULL)
         {
-            foundnode(x, p->brosis);
-            foundnode(x, p->bigchild);  
+            find(x, p->brosis);
+            find(x, p->bigchild);  
         }
-
-
     }
 }
-void addmember(string father,string member , node* p)
+//add function
+void addstart(string member)
+{
+    head->name=member;
+}
+void add(string father,string member , node* p)
 {
     if(p->name==father)
     {
         node* child = new node();
         child->name=member;
+        child->father=p;
         if(p->bigchild==NULL)
         {
             p->bigchild=child;
@@ -93,25 +120,49 @@ void addmember(string father,string member , node* p)
     {
         if(p->bigchild!=NULL && p->brosis==NULL)
         {
-            addmember(father, member, p->brosis);
+            add(father, member, p->bigchild);
         }
         if(p->bigchild==NULL && p->brosis!=NULL)
         {
-            addmember(father, member, p->bigchild);
+            add(father, member, p->brosis);
         }
         if(p->bigchild!=NULL && p->brosis!=NULL)
         {
-            addmember(father, member, p->brosis);
-            addmember(father, member, p->bigchild);  
+            add(father, member, p->brosis);
+            add(father, member, p->bigchild);  
         }
 
     }
 
 }
-void removemember (string member , node* p)
-{
 
+//continue
+
+void inhertitence (string x , node* p , int t)
+{
+    if(p->name==x)
+    {
+        t=1;
+    }
+    else
+    {
+        if(p->bigchild!=NULL && p->brosis==NULL)
+        {
+            inhertitence(x, p->bigchild , t);
+        }
+        if(p->bigchild==NULL && p->brosis!=NULL)
+        {
+            inhertitence(x, p->brosis , t);
+        }
+        if(p->bigchild!=NULL && p->brosis!=NULL)
+        {
+            inhertitence(x, p->brosis , t);
+            inhertitence(x, p->bigchild , t);  
+        }
+    }
+    
 }
+
 
 //A function
 bool isxgrandparent (string x , string y , int t , node* p)

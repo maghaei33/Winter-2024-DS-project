afb6c6e6fd9db476194f0bd3b12680df30f4c0fa
Alireza Khadem <akhasdf1383@gmail.com>
2024-01-25 10:09:40 +0330

    menu added

--- rproject.cpp
+++ rproject.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 struct node
 {
-    node *parent;
-    node *firstChild;
-    node *broSis;
+    node *parent = nullptr;
+    node *firstChild = nullptr;
+    node *broSis = nullptr;
     string name;
     int level;
 };
@@ -105,42 +105,28 @@ node *find(string x, node *p)
 void addstart(string member)
 {
     head->name = member;
+    head->level = 0;
 }
-void add(string parent, string member, node *p)
+void add(string parent, string member, int level)
 {
-    if (p->name == parent)
+    node *p = new node();
+    p = find(parent, head);
+    node *child = new node();
+    child->name = member;
+    child->parent = p;
+    child->level = level;
+    if (p->firstChild == NULL)
     {
-        node *child = new node();
-        child->name = member;
-        child->parent = p;
-        if (p->firstChild == NULL)
-        {
-            p->firstChild = child;
-        }
-        else
-        {
-            while (p->broSis != NULL)
-            {
-                p = p->broSis;
-            }
-            p->broSis = child;
-        }
+        p->firstChild = child;
     }
     else
     {
-        if (p->firstChild != NULL && p->broSis == NULL)
+        p = p->firstChild;
+        while (p->broSis != NULL)
         {
-            add(parent, member, p->firstChild);
+            p = p->broSis;
         }
-        if (p->firstChild == NULL && p->broSis != NULL)
-        {
-            add(parent, member, p->broSis);
-        }
-        if (p->firstChild != NULL && p->broSis != NULL)
-        {
-            add(parent, member, p->broSis);
-            add(parent, member, p->firstChild);
-        }
+        p->broSis = child;
     }
 }
 
@@ -177,44 +163,52 @@ bool isGrandParent(string x, string y)
 // isbroSis function
 bool isbroSis(string x, string y)
 {
-    int t_1 = 0;
-    int t_2 = 0;
+    // int t_1 = 0;
+    // int t_2 = 0;
     node *r_1 = new node();
     node *r_2 = new node();
     r_1 = find(x, head);
     r_2 = find(y, head);
-    while (r_1->broSis != NULL)
-    {
-        if (r_1->name == y)
-        {
-            t_1 = 1;
-            break;
-        }
-        else
-        {
-            r_1 = r_1->broSis;
-        }
-    }
-    while (r_2->broSis != NULL)
-    {
-        if (r_2->name == y)
-        {
-            t_2 = 1;
-            break;
-        }
-        else
-        {
-            r_2 = r_2->broSis;
-        }
-    }
-    if (t_1 == 1 || t_2 == 1)
+    if (r_1->parent == r_2->parent)
     {
         return true;
     }
-    if (t_1 == 0 && t_2 == 0)
+    else
     {
         return false;
     }
+    // while (r_1->broSis != NULL)
+    // {
+    //     if (r_1->name == y)
+    //     {
+    //         t_1 = 1;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         r_1 = r_1->broSis;
+    //     }
+    // }
+    // while (r_2->broSis != NULL)
+    // {
+    //     if (r_2->name == y)
+    //     {
+    //         t_2 = 1;
+    //         break;
+    //     }
+    //     else
+    //     {
+    //         r_2 = r_2->broSis;
+    //     }
+    // }
+    // if (t_1 == 1 || t_2 == 1)
+    // {
+    //     return true;
+    // }
+    // if (t_1 == 0 && t_2 == 0)
+    // {
+    //     return false;
+    // }
 }
 
 // distanRelation function
@@ -264,6 +258,12 @@ string findSameParent(string x, string y)
 int farthestBorn(string name)
 {
     // later
+}
+
+// farthest relationship of tree
+vector<string> farthestRelation()
+{
+    // not yet
 }
 
 int getChildCount(node *n)
@@ -510,33 +510,141 @@ void Visualize(string className, string params, bool Compile = true)
 
 int main()
 {
-    node *ali = new node();
-    node *mehdi = new node();
-    head->parent = nullptr;
-    head->name = "Alireza";
-    head->level = 1;
-    head->broSis = nullptr;
-    ali->parent = head;
-    ali->broSis = nullptr;
-    ali->name = "ali";
-    ali->level = 2;
-    head->firstChild = ali;
-    mehdi->parent = ali;
-    mehdi->broSis = nullptr;
-    mehdi->name = "mehdi";
-    mehdi->level = 3;
-    ali->firstChild = mehdi;
-    Encoder encoder = Encode(*(new Encoder(head)));
-    string Output = encoder.GetFullOutput();
-    Visualize("Graph", Output, true);
-    // while (true)
-    // {
-    //     cout << "Welcome to Family Tree!\n";
-    //     cout << "Choose appropriate number\n";
-    //     cout << "1) Add a new member to tree\n";
-    //     cout << "2) Remove a member from tree\n";
-    //     int command;
-    //     cin >> command;
-    // }
+    cout << "Welcome to Family Tree!\n";
+    while (true)
+    {
+        cout << "Choose appropriate number\n";
+        cout << "1) Add a new member to tree\n";
+        cout << "2) Remove a member from tree\n";
+        cout << "3) Show size of tree\n";
+        cout << "4) Check if A is parent of B\n";
+        cout << "5) Check if A is sibling of B\n";
+        cout << "6) Check if A has far relationship with B\n";
+        cout << "7) Find same grandparent\n";
+        cout << "8) Find farthest born of A\n";
+        cout << "9) Find farthest relationship\n";
+        cout << "10) Display tree\n";
+        int command;
+        cin >> command;
+        switch (command)
+        {
+        case 1:
+        {
+            // if 1 --------> adding new node to tree
+            string parent, name;
+            int level;
+            cout << "Enter parent, name, level: ";
+            cin >> parent >> name >> level;
+            if (parent == "null")
+            {
+                addstart(name);
+            }
+            else
+            {
+                add(parent, name, level);
+            }
+            break;
+        }
+        case 2:
+        {
+            // if 2 --------> delete a node from tree
+            string name;
+            cout << "Enter a name to delete: ";
+            cin >> name;
+            deleteMember(name);
+            break;
+        }
+        case 3:
+        {
+            // if 3 --------> find size of the tree
+            int counter = 0;
+            cout << "The tree has " << size(counter, head) << " members" << endl;
+            break;
+        }
+        case 4:
+        {
+            // if 4 --------> check if A is grandparent of B
+            string parent, name;
+            cout << "Enter parent and child name: ";
+            cin >> parent >> name;
+            if (isGrandParent(parent, name))
+            {
+                cout << "A is grandparent of B" << endl;
+            }
+            else
+            {
+                cout << "A is not grandparent of B" << endl;
+                break;
+            }
+        }
+        case 5:
+        {
+            // if 5 --------> check if A is sibling of B
+            string firstName, secondName;
+            cout << "Enter two names: ";
+            cin >> firstName >> secondName;
+            if (isbroSis(firstName, secondName))
+            {
+                cout << "They are siblings" << endl;
+            }
+            else
+            {
+                cout << "They are not siblings" << endl;
+            }
+            break;
+        }
+        case 6:
+        {
+            // if 6 --------> check if A has far relationship with B (they are not siblings or parent and child)
+            string firstName, secondName;
+            cout << "Enter two names: ";
+            cin >> firstName >> secondName;
+            if (distanRelation(firstName, secondName))
+            {
+                cout << "They have far relationship" << endl;
+            }
+            else
+            {
+                cout << "They dont have far relationship" << endl;
+            }
+            break;
+        }
+        case 7:
+        {
+            // if 7 --------> find same grandparent
+            string firstName, secondName;
+            cout << "Enter two names: ";
+            cin >> firstName >> secondName;
+            cout << firstName << " and " << secondName << " same parent is: " << findSameParent(firstName, secondName) << endl;
+            break;
+        }
+        case 8:
+        {
+            // if 8 --------> find farthest born of A
+            string name;
+            cout << "Enter name: ";
+            cin >> name;
+            cout << "The farthest born of " << name << " is " << farthestBorn(name) << endl;
+            break;
+        }
+        case 9:
+        {
+            // if 9 --------> find farthest relationship
+            vector<string> names = farthestRelation();
+            cout << names[0] << " and " << names[1] << " has farthest relationship in this tree" << endl;
+            break;
+        }
+        case 10:
+        {
+            // if 10 -------> visualize tree using Java
+            Encoder encoder = Encode(*(new Encoder(head)));
+            string Output = encoder.GetFullOutput();
+            Visualize("Graph", Output, true);
+            break;
+        }
+        default:
+            break;
+        }
+    }
     return 0;
 }
